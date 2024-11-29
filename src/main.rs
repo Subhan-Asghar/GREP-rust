55e33ecbda9ec2d2f7ae8c01241bada4006963ae
Subhan Asghar <subhanasghar16@gmail.com>
2024-11-30 00:00:55 +0500

    ch 12.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+    let query = &args[0];
+    // let file_path = &args[1];
+
+    println!("Searching for {query}");
+    // println!("In file {file_path}");
+    println!("the strung is {:?}",args);
 }
